a99dd30ab65f96ac3b73e33c6e70ac22631e037d
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-11-16 01:12:47 +0100

    add record and save video to library functionality

--- RWVideoNinja/RecordVideoViewController.swift
+++ RWVideoNinja/RecordVideoViewController.swift
@@ -31,8 +31,49 @@
 /// THE SOFTWARE.
 
 import UIKit
+import MobileCoreServices
 
 class RecordVideoViewController: UIViewController {
   @IBAction func record(_ sender: AnyObject) {
+    VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
   }
+  
+  @objc func video(_ videoPath: String,
+                   didFinishavingWithError error: Error?,
+                   contextInfo info: AnyObject) {
+    let title = (error == nil) ? "Success" : "Error"
+    let message = (error == nil) ? "Video was saved." : "Video failed to save."
+    
+    let alert = UIAlertController(
+      title: title,
+      message: message,
+      preferredStyle: .alert
+    )
+    alert.addAction(UIAlertAction(title: "Ok",
+                                  style: UIAlertAction.Style.cancel,
+                                  handler: nil))
+    present(alert, animated: true, completion: nil)
+  }
+}
+
+extension RecordVideoViewController: UIImagePickerControllerDelegate {
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    dismiss(animated: true, completion: nil)
+    
+    guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+          mediaType == (kUTTypeMovie as String),
+          let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL,
+          UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path) else {
+      return
+    }
+    
+    UISaveVideoAtPathToSavedPhotosAlbum(url.path,
+                                        self,
+                                        #selector(video(_:didFinishavingWithError:contextInfo:)),
+                                        nil)
+  }
+}
+
+extension RecordVideoViewController: UINavigationControllerDelegate {
+  
 }
